475c6b18d87cc5d2ec0b648a5c853f31f42e2fbf
yooundong-jp <imyour8572@gmail.com>
2025-11-17 14:57:35 +0900

    Init ListArk Spring Boot project

---
+++ src/main/java/com/example/ListArk/ListArkApplication.java
@@ -0,0 +1,13 @@
+package com.example.ListArk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ListArkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ListArkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ListArk/ListArkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ListArk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ListArkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
